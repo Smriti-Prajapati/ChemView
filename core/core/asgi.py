bca0dc4c800f612dd6ad8fe85ede5ab5a01ff1be
SMRITI PRAJAPATI <143401846+Smriti-Prajapati@users.noreply.github.com>
2026-01-31 22:59:15 +0530

    Fix Django paths and Render deployment config

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -3,7 +3,7 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE',
-    'core.core.settings'
+    'core.settings'
 )
 
 application = get_asgi_application()
--- core/core/wsgi.py
+++ core/core/wsgi.py
@@ -3,7 +3,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE',
-    'core.core.settings'
+    'core.settings'
 )
 
 application = get_wsgi_application()
